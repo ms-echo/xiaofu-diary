5a1cbe788d6e851f6b236c80bf3a9d8f78fc2897
zzhangxinyue110-collab <zzhangxinyue110@gmail.com>
2026-09-18 09:54:14 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,1 @@
+self.addEventListener('install',e=>e.waitUntil(caches.open('xiaofu-github-v1').then(c=>c.addAll(['./','./index.html','./style.css','./app.js','./manifest.webmanifest','./icon.png','./mascot-happy.png','./mascot-waiting.png']))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
