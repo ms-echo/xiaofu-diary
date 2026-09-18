f7dfb8db4c41ab27b2358924326d9b6cb8a9f9c1
zzhangxinyue110-collab <zzhangxinyue110@gmail.com>
2026-09-18 11:02:15 +0800

    强制刷新网页缓存

--- sw.js
+++ sw.js
@@ -1,1 +1,5 @@
-self.addEventListener('install',e=>e.waitUntil(caches.open('xiaofu-github-v1').then(c=>c.addAll(['./','./index.html','./style.css','./app.js','./manifest.webmanifest','./icon.png','./mascot-happy.png','./mascot-waiting.png']))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE='xiaofu-github-v2';
+const ASSETS=['./','./index.html','./style.css','./app.js','./supabase-config.js','./manifest.webmanifest','./icon.png','./mascot-happy.png','./mascot-waiting.png'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{let c=r.clone();caches.open(CACHE).then(cache=>cache.put(e.request,c));return r}).catch(()=>caches.match(e.request)))});
