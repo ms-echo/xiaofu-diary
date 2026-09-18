8f19965073f6f728c29ad78312ddd06ed0f35400
zzhangxinyue110-collab <zzhangxinyue110@gmail.com>
2026-09-18 10:51:19 +0800

    接入邮箱登录和云端同步

---
+++ supabase-config.js
@@ -0,0 +1,3 @@
+// 仅包含可公开的网页连接信息；不包含数据库密码或管理密钥。
+window.XIAOFU_SUPABASE_URL = 'https://elbaqqkuvlqgltbpfpci.supabase.co';
+window.XIAOFU_SUPABASE_KEY = 'sb_publishable_E_bTOhJQmUS31rhTCvxwSg_Nm6FgXxu';
